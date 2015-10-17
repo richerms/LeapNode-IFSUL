132572146598377622a8f8144c8bb19dad0155ca
richerms <richerms@hotmail.com>
2015-10-17 00:26:06 -0300

    Coleta de Dados

--- Executable/coletadedados.js
+++ Executable/coletadedados.js
@@ -2,7 +2,9 @@ var tracking = false;
 var scan = false;
 var metadeDosDedos = [];
 var gestureID;
-var banco = '{ "Eixos (X,Y,Z)" : [';
+var r = 0;
+var enviar = false;
+var flag = true;
 
 //função da distância da palma      
 function calcDistancia(A, B) {
@@ -14,18 +16,40 @@ function iniciar(){
 	if (document.getElementById("Start").innerHTML === "Start") {
 		document.getElementById("Start").innerHTML = "Stop";
 		tracking = true;
+		document.getElementById("Gesture").innerHTML = "I";		
+		document.getElementById("gimg").src = "img" + r + ".jpg";
+		r++;
 	}
 	else {
 		document.getElementById("Start").innerHTML = "Start";
 		scan = false;
+		r = 0;
+		document.getElementById("gimg").src = "img" + r + ".jpg";
 	}
 }
 
-function gravar(){
-	document.getElementById("dados").href = "http://localhost:8080/?s=" + banco + "]}";
-	banco = '{ "Eixos (X,Y,Z)" : [';
-	var dataehora = new Date();
-	//document.getElementById("aviso").innerHTML = "Última captura feita às:  " + dataehora.getHours() + 'h' + dataehora.getMinutes() + 'min' + '<br>Pronta para Download!';
+function next(){
+		enviar = true;
+}
+
+function save(){
+	document.getElementById("gimg").src = "img" + r + ".jpg";
+	
+	var shyriu = document.getElementById("Gesture");
+
+	switch (r){
+		case 0: shyriu.innerHTML = "I";		r++;break
+		case 1: shyriu.innerHTML = "B"; 	r++;break
+		case 2: shyriu.innerHTML = "8"; 	r++;break
+		case 3: shyriu.innerHTML = "2"; 	r++;break
+		case 4: shyriu.innerHTML = "Open"; 	r++;break
+		case 5: shyriu.innerHTML = "Close";	r=0;break 
+	}
+
+}
+
+function Data(){
+	
 }
 
 function atualizarDados(hand) {
@@ -35,7 +59,7 @@ function atualizarDados(hand) {
 	for (var i = 0; i < 5; i++){
 		document.getElementById("finger" + i).innerHTML = hand.fingers[i].dipPosition;
 		document.getElementById("distance" + i).innerHTML = calcDistancia(hand.fingers[i].dipPosition, hand.palmPosition);
-		banco += "Dedo" + i + ":" + hand.fingers[i].dipPosition + ",";
+	//	banco += "Dedo" + i + ":" + hand.fingers[i].dipPosition + ",";
 		if (calcDistancia(hand.fingers[i].dipPosition, hand.palmPosition) < metadeDosDedos[i]){
 			document.getElementById("state" + i).innerHTML = "DOWN";
 		}
@@ -59,42 +83,6 @@ function objToString (obj) {
 }
 
 
-function analizarGesto() {
-	document.getElementById("gid").innerHTML = gestureID;
-	var name = document.getElementById("gn");
-	var image = document.getElementById("gimg");
-	
-	switch (gestureID){
-		case 1:
-			name.innerHTML = "OK";
-			image.src = "http://www.nocturnar.com/comunidad/attachments/ok-jpg.62283/";
-			break;
-		case 10:
-			name.innerHTML = "ONE";
-			image.src = "https://s3-sa-east-1.amazonaws.com/leroy-production//uploads/img/products/numero_para_residencia_numero_1_14_5_cmx10_cm_cromado_bemfixa_87963701_0001.jpg_600x600.jpg";
-			break;
-		case 110:
-			name.innerHTML = "TWO";
-			image.src = "https://s3-sa-east-1.amazonaws.com/leroy-production//uploads/img/products/numero_para_residencia_numero_2_14_5_cmx10_cm_cromado_bemfixa_87963715_0001.jpg_600x600.jpg";
-			break;
-		case 1110:
-			name.innerHTML = "THREE";
-			image.src = "https://s3-sa-east-1.amazonaws.com/leroy-production//uploads/img/products/numero_para_residencia_numero_3_14_5_cmx10_cm_cromado_bemfixa_87963722_0001.jpg_600x600.jpg";
-			break;
-		case 11110:
-			name.innerHTML = "FOUR";
-			image.src = "https://s3-sa-east-1.amazonaws.com/leroy-production//uploads/img/products/numero_para_residencia_numero_4_14_5_cmx10_cm_cromado_bemfixa_87963736_0001.jpg_600x600.jpg";
-			break;
-		case 11111:
-			name.innerHTML = "FIVE";
-			image.src = "https://s3-sa-east-1.amazonaws.com/leroy-production//uploads/img/products/numero_para_residencia_numero_5_14_5_cmx10_cm_cromado_bemfixa_87963743_0001.jpg_600x600.jpg";
-			break;
-		default:
-			name.innerHTML = "Não Reconhecido";
-			image.src = "";
-	}
-}
-
 //Existe uma função extend da classe finger que poderia executar essa função apenas para dedo em pé e dobrado.
 function calibrar(hand) {
 	for (var i = 0; i < 5; i++){
@@ -107,8 +95,7 @@ function calibrar(hand) {
 
 //Tem que preparar o readwrite para aceitar todas as propriedades que estou enviando
 function enviarDB(hand){
-	var str = 'timeStamp=' + Date.now() + '&gesture=' + "Nenhum nome";
-	
+	var str = 'timeStamp=' + Date.now() + '&gesture=' + document.getElementById("Gesture").innerHTML;	
 	for (var j = 0; j < 3; j++)
 		str += '&palmPosition'+ j + '=' + hand.palmPosition[j];
 	
@@ -130,7 +117,10 @@ function enviarDB(hand){
 	xhttp.open("POST", "http://localhost:8080/save", true);
 	xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
 	xhttp.send(str);
-	iniciar();
+	
+    if (xhttp.readyState == 4 && xhttp.status == 200) {
+     // document.getElementById("aviso").innerHTML = xhttp.responseText;
+    }
 }
 
 //main function para o tracking
@@ -142,11 +132,14 @@ Leap.loop(function(frame) {
 			
 		if (scan) {
 			atualizarDados(hand);
-			analizarGesto();
+//			analizarGesto();
 			console.log("Analisei o Gesto");
-			//document.getElementById("aviso").innerHTML = objetoDados(hand);
-			
+//			document.getElementById("aviso").innerHTML = objetoDados(hand);
+		}	
+		if (enviar){
 			enviarDB(hand);
-		}		
+			enviar = false;
+			save();
+		}
 	}
 )});
--- Executable/readwrite.js
+++ Executable/readwrite.js
@@ -3,52 +3,131 @@
  */
 var express = require('express')
 var fs = require('fs');
+var app = express();
+var sqlite3 = require('sqlite3').verbose();
+var db = new sqlite3.Database('carmen.db');
+var bodyParser = require('body-parser');
 
-var app = express()
+var TABLE_NAME = 'gestures';
+var TABLE_CONTENT = [
+		['id', 'INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT'],
+		['timestamp', 'INTEGER'],
+		['gesture', 'TEXT'],
+		['palmPositionX', 'REAL'],
+		['palmPositionY', 'REAL'],
+		['palmPositionZ', 'REAL'],
+		['fingers0PositionX', 'REAL'],
+		['fingers0PositionY', 'REAL'],
+		['fingers0PositionZ', 'REAL'],
+		['fingers1PositionX', 'REAL'],
+		['fingers1PositionY', 'REAL'],
+		['fingers1PositionZ', 'REAL'],
+		['fingers2PositionX', 'REAL'],
+		['fingers2PositionY', 'REAL'],
+		['fingers2PositionZ', 'REAL'],
+		['fingers3PositionX', 'REAL'],
+		['fingers3PositionY', 'REAL'],
+		['fingers3PositionZ', 'REAL'],
+		['fingers4PositionX', 'REAL'],
+		['fingers4PositionY', 'REAL'],
+		['fingers4PositionZ', 'REAL'],
+		['fingers0Distance', 'REAL'],
+		['fingers1Distance', 'REAL'],
+		['fingers2Distance', 'REAL'],
+		['fingers3Distance', 'REAL'],
+		['fingers4Distance', 'REAL'],
+		['distanceBetweenFinger0AndFinger1', 'REAL'],
+		['distanceBetweenFinger0AndFinger2', 'REAL'],
+		['distanceBetweenFinger0AndFinger3', 'REAL'],
+		['distanceBetweenFinger0AndFinger4', 'REAL'],
+		['distanceBetweenFinger1AndFinger0', 'REAL'],
+		['distanceBetweenFinger1AndFinger2', 'REAL'],
+		['distanceBetweenFinger1AndFinger3', 'REAL'],
+		['distanceBetweenFinger1AndFinger4', 'REAL'],
+		['distanceBetweenFinger2AndFinger0', 'REAL'],
+		['distanceBetweenFinger2AndFinger1', 'REAL'],
+		['distanceBetweenFinger2AndFinger3', 'REAL'],
+		['distanceBetweenFinger2AndFinger4', 'REAL'],
+		['distanceBetweenFinger3AndFinger0', 'REAL'],
+		['distanceBetweenFinger3AndFinger1', 'REAL'],
+		['distanceBetweenFinger3AndFinger2', 'REAL'],
+		['distanceBetweenFinger3AndFinger4', 'REAL'],
+		['distanceBetweenFinger4AndFinger0', 'REAL'],
+		['distanceBetweenFinger4AndFinger1', 'REAL'],
+		['distanceBetweenFinger4AndFinger2', 'REAL'],
+		['distanceBetweenFinger4AndFinger3', 'REAL']
+	];	//Eu podia fazer isso de um jeito muito mais eficiente, mas estava com preguiça de pensar
 
-ROOT_APP_PATH = fs.realpathSync('.'); 
-console.log(ROOT_APP_PATH);
+app.use(bodyParser.urlencoded({extended: false}));
+
+//Verifica o banco de dados. Cria a tabela se não a encontrar.
+db.get('SELECT * FROM ' + TABLE_NAME, function(err, row) {
+	if (err != null) {
+		var content ='';
+		
+		for (var i = 0; i < TABLE_CONTENT.length; i++){
+			content += '"' + TABLE_CONTENT[i][0] + '" ' + TABLE_CONTENT[i][1];
+			if (i < TABLE_CONTENT.length - 1)
+				content += ', ';
+		}
+		
+		db.run('CREATE TABLE "' + TABLE_NAME + '" (' + content + ')', 
+		function(err) {
+			if (err != null) {
+				console.log(err);
+			}
+			else {
+				console.log('Tabela ' + TABLE_NAME + ' não encontrada.');
+				console.log('Tabela criada.');
+			}
+		})
+	}
+	else {
+		console.log('Tabela ' + TABLE_NAME + ' encontrada.');
+	}
+});
 
 app.get("/", function(req, res){
 	
-	salvaArquivo(req.param('s'));
-	res.end("Sucesso!\nArquivo salvo como: \"dados.json\" \n\nSuccess!\nFile saved as \"dados.json\"" );
-	 
+});
 
+app.post("/save", function(req, res){
+	console.log('Request for /save received.')
+	
+	//console.log(req.body);
+	
+	var content = '';
+	
+	for (var i = 1; i < TABLE_CONTENT.length; i++){
+			content += TABLE_CONTENT[i][0];
+			if (i < TABLE_CONTENT.length - 1)
+				content += ', ';
+		}
+		
+	//console.log(content);
+	
+	var data = '';
+	
+	for (var p in req.body)
+		if (req.body.hasOwnProperty(p)){
+			data += '"' + req.body[p] + '", ';
+		}
+			
+	data = data.substring(0, data.length - 2);
+	
+	//console.log(data);
+	
+	db.run('INSERT INTO ' + TABLE_NAME + ' (' + content + ') VALUES (' + data + ')',
+			function (err) {
+				if (err != null){
+					console.log(err);
+					res.send("Erro ao salvar os dados.");
+				}
+				else
+					res.send("Dados salvos com sucesso.");
+			});
 })
 
-app.get("/learquivo", function(req, res){
-	console.log(req.query);
-	leArquivo(res);
-	 
+app.listen(8080);
 
-})
-
-app.post("/", function(req, res){
-	res.end("Olá mundo")
-
-})
-app.listen(8080)
 console.log("Servidor rodando...")
-
-
-function salvaArquivo(string){
-	fs.writeFile("dados.json", string, function(err) {
-	    if(err) {
-	        return console.log(err);
-	    }
-
-	    console.log("The file was saved!");
-	});
-}
-
-function leArquivo(res){
-	var arquivo = "";
-	fs.readFile('test.txt', 'utf8', function (err,data) {
-	  if (err) {
-	    return console.log(err);
-	  }
-	  console.log(data);
-	  res.end(data);
-	});
-}
